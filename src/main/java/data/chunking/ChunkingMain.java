db849e184021ce42839997200787324932486a53
Leon <leonbornemann@gmx.de>
2016-04-02 15:09:03 +0200

    Changed some stuff, added dummy files so the directories get commited,

--- src/main/java/data/chunking/ChunkingMain.java
+++ src/main/java/data/chunking/ChunkingMain.java
@@ -7,8 +7,9 @@ import java.text.ParseException;
 public class ChunkingMain {
 
 	public static void main(String[] args) throws IOException, ParseException{
-		File originFile = new File("D:\\Documents\\Uni\\Master thesis\\Datasets\\DEBS 2015\\sorted_data_complete.csv");
+		//we absolutely don't want this to be run accidentally so we outcomment it :D
+		/*File originFile = new File("D:\\Documents\\Uni\\Master thesis\\Datasets\\DEBS 2015\\sorted_data_complete.csv");
 		File targetDir = new File("D:\\Documents\\Uni\\Master thesis\\Datasets\\DEBS 2015\\Chunked Data");
-		new DataChunker(originFile, targetDir).chunk();
+		new DataChunker(originFile, targetDir).chunk();*/
 	}
 }
